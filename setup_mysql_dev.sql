2cc1c9c6791ac331b9a86d6b2d68c29beed8efd6
dt <dtvangogh@github.com>
2020-08-11 16:27:00 +0000

    remove extraneous newline

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,4 @@
 -- create hbnb_dev_db
-
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
