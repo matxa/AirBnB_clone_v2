6e701073ad0db4db2938073cb363bd8c42ff1b07
dt <dtvangogh@github.com>
2020-08-12 06:20:59 +0000

    city and state

--- models/city.py
+++ models/city.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    """ The city class, contains state ID and name
+    if a state is deleted, all cities tied to state also deleted
+    """
+
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete-orphan')
--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
+from models.base_model import Base
+from sqlalchemy.orm import relationship
+from models.city import City
+import os
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            'City',
+            cascade="all,delete-orphan",
+            backref="state")
+    else:
+        name = ""
+    
+        @property
+        def cities(self):
+            """"""
+            cityArray = []
+            for item in models.storage.all(City).values():
+                if item.state_id == self.id:
+                    cityArray.append(item)
+            return cityArray
