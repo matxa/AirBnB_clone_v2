174122282809733f83b7f594a4da423eba4dccd2
dt <dtvangogh@github.com>
2020-08-13 17:22:16 +0000

    add"

--- models/city.py
+++ models/city.py
@@ -17,7 +17,7 @@ class City(BaseModel, Base):
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
         places = relationship('Place', cascade='all, delete-orphan',
-                             backref='cities')
+                              backref='cities')
     else:
         name = ""
         state_id = ""
