02f1ea6c563249c71603cbb37b4011ca49d69e3c
dt <dtvangogh@github.com>
2020-08-13 02:02:03 +0000

    remove rleationships until more understanding

--- models/place.py
+++ models/place.py
@@ -26,10 +26,6 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         amenity_ids = []
-        user = relationship("User")
-        cities = relationship("City")
-        reviews = relationship("Review", cascade="all, delete-orphan")
-        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False, back_populates="place_amenities")
 
     else:
         city_id = ""
