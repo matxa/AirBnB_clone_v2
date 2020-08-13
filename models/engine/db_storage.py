80402c77ee8e8368501b92592a991b461880c978
dt <dtvangogh@github.com>
2020-08-13 02:06:48 +0000

    add place to query

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -55,7 +55,7 @@ class DBStorage:
             query_data.append(self.__session.query(State).all())
             query_data.append(self.__session.query(City).all())
             # query_data.append(self.__session.query(Amenity).all())
-            # query_data.append(self.__session.query(Place).all())
+            query_data.append(self.__session.query(Place).all())
             # query_data.append(self.__session.query(Review).all())
 
         dict_objs = {}
