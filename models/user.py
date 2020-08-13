4a8fc75d1f852b1ea140983f4421766f9d4f094d
dt <dtvangogh@github.com>
2020-08-13 01:34:10 +0000

    user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    '''
+        Definition class User 
+    '''
+    __tablename__ = 'users'
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
