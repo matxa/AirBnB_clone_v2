eb54db0061d8be70b764da6bf5cd0bdb521ce6f1
dt <dtvangogh@github.com>
2020-08-13 17:20:04 +0000

    relationships

--- models/city.py
+++ models/city.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
 
 
 class City(BaseModel, Base):
@@ -12,5 +13,11 @@ class City(BaseModel, Base):
     """
 
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship('Place', cascade='all, delete-orphan',
+                             backref='cities')
+    else:
+        name = ""
+        state_id = ""
