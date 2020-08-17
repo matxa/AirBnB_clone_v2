8a932cde8fc4bbc2a8b25d163728a163092582e8
matxa <42968545+matxa@users.noreply.github.com>
2020-08-17 19:37:34 -0400

    setup 3

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 # Install nginx and prepare server for web_static
 
-apt-get -y update
-apt-get -y install nginx
-sudo systemctl start nginx
-mkdir -p /data
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "<h1>Testing Nginx config</h1>" > /data/web_static/releases/test/index.html
-ln -sfn data/web_static/current /data/web_static/releases/test
-chown ubuntu:ubuntu /data/
-chown g+x /data/
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo sudo systemctl start nginx
+sudo mkdir -p /data
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "<h1>Testing Nginx config</h1>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sfn data/web_static/current /data/web_static/releases/test
+sudo chown -R ubuntu:ubuntu /data/
 LINE="location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}"
-sed -i "54i $LINE" /etc/nginx/sites-available/default
-service nginx restart
+sudo sed -i "54i $LINE" /etc/nginx/sites-available/default
+sudo service nginx restart
