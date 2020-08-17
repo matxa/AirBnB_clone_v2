331ec69f67af73fcfd9f066a7c9358fe4802b469
matxa <42968545+matxa@users.noreply.github.com>
2020-08-17 19:20:40 -0400

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,14 +3,16 @@
 
 apt-get -y update
 apt-get -y install nginx
+sudo systemctl start nginx
 mkdir /data
 mkdir /data/web_static/
 mkdir /data/web_static/releases/
 mkdir /data/web_static/shared/
 mkdir /data/web_static/releases/test/
-echo "Testing Nginx config" > /data/web_static/releases/test/index.html
+echo "<h1>Testing Nginx config</h1>" > /data/web_static/releases/test/index.html
 ln -sfn data/web_static/current /data/web_static/releases/test
-chown ubuntu:nginx /data/
+chown ubuntu:ubuntu /data/
 chown g+x /data/
-sed -i "/listen 80 default_server;/a rewrite ^/hbnb_static /data/web_static/current/ permanent;" /etc/nginx/sites-available/default
+LINE="location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}"
+sed -r "54i $LINE" /etc/nginx/sites-available/default
 service nginx restart
