fd7ebbf5276626bbc5f1834d726600adfef497a6
matxa13 <matxa21@gmail.com>
2020-08-17 21:34:17 -0400

    make it

---
+++ 1-pack_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from fabric.api import local
+from datetime import datetime
+import os.path
+from os import path
+
+time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
+tar_archive = "web_static_" + time_stamp + ".tgz"
+
+def do_pack():
+    if path.exists('versions') is False:
+        local("mkdir versions")
+    local("tar -cvzf versions/{} web_static".format(tar_archive))
