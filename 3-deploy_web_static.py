bd1bc348defd0442ba0975775a8a46f238af2abe
matxa13 <matxa21@gmail.com>
2020-08-17 23:54:15 -0400

    func

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,32 +7,14 @@ from datetime import datetime
 import os.path
 from os import path
 
-time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
-tar_archive = "web_static_" + time_stamp + ".tgz"
+
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
-def do_pack():
-    """run fabric on local"""
-    if path.exists('versions') is False:
-        local("mkdir versions")
-    local("tar -cvzf versions/{} web_static".format(tar_archive))
-
-
-env.hosts = ["34.75.185.85", "34.73.88.228"]
-
-
-def do_deploy(archive_path):
-    """deploy to server"""
-    if path.exists('versions') is False:
+def deploy():
+    """deploy"""
+    if not do_pack():
         return False
-    try:
-        put('versions/web_static_20200817213142.tgz', '/tmp/web_static_20200817213142.tgz')
-        run("tar -xzvf /tmp/web_static_20200817213142.tgz \
-            /data/web_static/releases/web_static_20200817213142")
-        run("rm -rf /tmp/web_static_20200817213142.tgz")
-        run("rm /data/web_static/current")
-        run("sudo ln -sf /data/web_static/releases/web_static_20200817213142/ \
-            /data/web_static/current")
-        return True
-    except Exception as e:
-        return False
+    b = do_deploy('versions/web_static_20200817213142.tgz')
+    return b
