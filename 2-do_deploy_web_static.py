862887ad5650aed729d9e2e43be70869283f4c31
matxa13 <matxa21@gmail.com>
2020-08-21 12:00:59 -0400

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,14 +13,12 @@ def do_deploy(archive_path):
     """deploy to server"""
     if path.exists('versions') is False:
         return False
-    try:
-        put('/tmp/', 'web_static_20200817213142.tgz')
-        run("tar -xzvf /tmp/web_static_20200817213142.tgz \
-            /data/web_static/releases/web_static_20200817213142")
-        run("rm -rf /tmp/web_static_20200817213142.tgz")
-        run("rm /data/web_static/current")
-        run("sudo ln -sf /data/web_static/releases/web_static_20200817213142/ \
-            /data/web_static/current")
-        return True
-    except Exception as e:
-        return False
+    f_no_ext = archive_path[archive_path.find('/') + 1:archive_path.find('.')]
+    f_yes_ext = archive_path[archive_path.find('/') + 1:]
+    put(archive_path, '/tmp/')
+    run("tar -xzvf /tmp/{} /data/web_static/{}".format(f_yes_ext, f_no_ext))
+    run("rm -rf /tmp/{}".format(f_yes_ext))
+    run("rm -rf /data/web_static/current")
+    run("sudo ln -sf /data/web_static/releases/{}/ \
+        /data/web_static/current".format(f_no_ext))
+    return True
