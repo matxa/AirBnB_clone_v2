e05621e8c497c85ad0eec0852fe86475c28a7678
matxa13 <matxa21@gmail.com>
2020-08-21 12:19:49 -0400

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-from fabric.api import local
-from fabric.api import env
-from fabric.operations import run, put
+from fabric.api import *
 import os.path
 from os import path
 
-env.hosts = ["34.75.185.85", "34.73.88.228"]
+env.hosts = ['34.75.185.85', '34.73.88.228']
 
 
 def do_deploy(archive_path):
@@ -15,10 +13,11 @@ def do_deploy(archive_path):
         return False
     f_no_ext = archive_path[archive_path.find('/') + 1:archive_path.find('.')]
     f_yes_ext = archive_path[archive_path.find('/') + 1:]
-    put(archive_path, '/tmp/')
-    run("tar -xzvf /tmp/{} /data/web_static/{}".format(f_yes_ext, f_no_ext))
-    run("rm -rf /tmp/{}".format(f_yes_ext))
-    run("rm -rf /data/web_static/current")
+    put(archive_path, "/tmp/")
+    run("sudo mkdir -p /data/web_static/releases/{}/".format(f_no_ext))
+    run("sudo tar -xzvf /tmp/{} /data/web_static/{}".format(f_yes_ext, f_no_ext))
+    run("sudo rm -rf /tmp/{}".format(f_yes_ext))
+    run("sudo rm -rf /data/web_static/current")
     run("sudo ln -sf /data/web_static/releases/{}/ \
         /data/web_static/current".format(f_no_ext))
     return True
