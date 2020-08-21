2433b2b019bdbda4acb9790f7e098f54378a6fce
matxa13 <matxa21@gmail.com>
2020-08-21 16:36:27 -0400

    s

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Deploy"""
 from fabric.api import local
 from fabric.api import env
 from fabric.operations import run, put
@@ -10,11 +10,14 @@ from os import path
 
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
+env.hosts = ['34.75.185.85', '34.73.88.228']
 
 
 def deploy():
     """deploy"""
-    if not do_pack():
+    path_name = do_pack()
+    if not path_name:
         return False
-    b = do_deploy('versions/web_static_20200817213142.tgz')
-    return b
+
+    value = do_deploy(path_name)
+    return value
