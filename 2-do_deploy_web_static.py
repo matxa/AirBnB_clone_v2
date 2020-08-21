2e8f8634cf3da53ad0735d4c22f1cb2e41a4316d
matxa <42968545+matxa@users.noreply.github.com>
2020-08-21 12:24:31 -0400

    de

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Deploy webstack"""
 from fabric.api import *
 import os.path
 from os import path
@@ -15,7 +15,8 @@ def do_deploy(archive_path):
     f_yes_ext = archive_path[archive_path.find('/') + 1:]
     put(archive_path, "/tmp/")
     run("sudo mkdir -p /data/web_static/releases/{}/".format(f_no_ext))
-    run("sudo tar -xzvf /tmp/{} /data/web_static/{}".format(f_yes_ext, f_no_ext))
+    run("sudo tar -xzvf /tmp/{} /data/web_static/{}".format(
+        f_yes_ext, f_no_ext))
     run("sudo rm -rf /tmp/{}".format(f_yes_ext))
     run("sudo rm -rf /data/web_static/current")
     run("sudo ln -sf /data/web_static/releases/{}/ \
