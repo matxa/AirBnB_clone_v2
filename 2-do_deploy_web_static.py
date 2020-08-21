d51f320c98576d53d83f1f2cb60759ca898a824f
matxa <42968545+matxa@users.noreply.github.com>
2020-08-21 14:19:56 -0400

    new m

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,13 +17,13 @@ def do_deploy(archive_path):
         f_y = archive_path[archive_path.find('/') + 1:]
         put(archive_path, "/tmp/")
         run("sudo mkdir -p /data/web_static/releases/{}/".format(f_n))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/{}".format(
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
             f_y, f_n))
         run("sudo rm /tmp/{}".format(f_y))
         run("sudo mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/".format(f_n, f_n))
         run("sudo rm -rf /data/web_static/releases/{}/web_static".format(f_n))
-        run("rm -rf /data/web_static/current")
+        run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s /data/web_static/releases/{}/ \
             /data/web_static/current".format(f_n))
         return True
