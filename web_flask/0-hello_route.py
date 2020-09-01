a968a77f07881f17c1a38a17764bd342392740e1
matxa <42968545+matxa@users.noreply.github.com>
2020-09-01 15:46:41 -0400

    restructure file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Helo route
+"""
 from flask import Flask
 
 
@@ -7,7 +9,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """hello route"""
     return "Hello HBNB!"
 
 
-app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
