8f52fc7feaaebe2461a3b40145e2cc43b57cd24f
matxa <42968545+matxa@users.noreply.github.com>
2020-09-01 15:50:46 -0400

    new route for /hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-from web_flask import app
+"""HBNB route
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """hbnb route"""
     return "HBNB"
 
 
