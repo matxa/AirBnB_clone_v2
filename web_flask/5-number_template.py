60eb3e00f97f8b5c3971f792e02f606c585f9de2
matxa <42968545+matxa@users.noreply.github.com>
2020-09-01 16:03:31 -0400

    using jinja to add to template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-from web_flask import app
-from flask import render_template
+"""render template
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def template(n):
+    """render template"""
     return render_template('5-number.html', n=n)
 
 
