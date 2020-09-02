93028f5de56a79a508928b3e8ca69796630bf5d2
matxa <42968545+matxa@users.noreply.github.com>
2020-09-02 00:02:24 -0400

    getting data from db

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 import os
+from flask import g
 
 
 app = Flask(__name__)
@@ -13,16 +14,12 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def render_state_obj():
     """render html list"""
-    # if os.getenv("HBNB_TYPE_STORAGE") == "db":
     list_of_states = storage.all(State)
-    storage.close()
     return render_template('7-states_list.html', states=list_of_states)
 
-
 @app.teardown_appcontext
-def teardown_db():
+def teardown_db(self):
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
